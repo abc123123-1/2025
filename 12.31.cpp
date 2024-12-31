c4a689b46e7f2aa1aacec505756ba9b690d1014a
stone'shi <stone'shi@LAPTOP-3R6SLTE4>
2024-12-31 20:05:29 +0800

    添加项目文件。

---
+++ 12.31.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int math() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    int x;
+    int y;
+    cin >> x;
+    y = a * x * x + b * x + c;
+    cout << "y= " << y << endl;
+
+    return 0;
+}
+int main()
+{
+    math();
+    return 0;
+}
+
